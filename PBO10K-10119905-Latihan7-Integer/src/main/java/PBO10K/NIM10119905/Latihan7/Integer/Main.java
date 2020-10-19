f1cfeee09481ebd6b625c114464bc899febbddaf
arumayara <72974847+arumayara@users.noreply.github.com>
2020-10-19 07:14:28 +0700

    Add files via upload

---
+++ PBO10K-10119905-Latihan7-Integer/src/main/java/PBO10K/NIM10119905/Latihan7/Integer/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119905.Latihan7.Integer;
+
+/**
+ *
+ * @author 
+ * NAMA    : Abraham Rumayara
+ * KELAS   : PBO10K
+ * NIM     : 10119905
+ * Deskripsi Program : Prgram ini menampilan tipe data Integer
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        
+        System.out.println("byte : "+b);
+        System.out.println("short : "+s);
+        System.out.println("int: "+ i);
+        System.out.println("long: "+ l);
+        
+    }
+    
+}
